87314d177721e9b3f0152a3b8e833b7715489b31
Brandon Hsu <55924012+branboyo@users.noreply.github.com>
2021-01-07 15:35:57 -0500

    Added phrase time limit and timeout duration

--- stt.py
+++ stt.py
@@ -3,13 +3,11 @@ import speech_recognition as sr
 def main():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=1)
+        r.adjust_for_ambient_noise(source)
         print("Listening...")   
-
-        audio = r.listen(source)
+        audio = r.listen(source,timeout=3, phrase_time_limit=3)
         try:
-            print("You said : {}".format(text))
-            text = r.recognize_google(audio)
+            print("You said : " + r.recognize_google(source))
         except Exception as e:
             print("Error:" + str(e))
 
