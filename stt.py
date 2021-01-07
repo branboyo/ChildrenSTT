3f1e230ae285403cc9d0ba542c89a06c648d2d50
Brandon Hsu <55924012+branboyo@users.noreply.github.com>
2021-01-07 17:47:30 -0500

    Requires microphone access in order for it to function

--- stt.py
+++ stt.py
@@ -1,14 +1,31 @@
 import speech_recognition as sr
-
+# Requires python 3.0+
+# pip3 install pyaudio
+# pip3 install speechrecognition
+# 
+ 
 def main():
+ 
     r = sr.Recognizer()
+ 
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
-        print("Listening...")   
-        audio = r.listen(source,timeout=3, phrase_time_limit=3)
+ 
+        print("Listening...")
+ 
+        audio = r.listen(source, phrase_time_limit=2)
+ 
         try:
-            print("You said : " + r.recognize_google(source))
+            if (r.recognize_google(audio) == "Daniel is gay"):
+                print("Very true brother")
+            else:
+                print("You have said \n" + r.recognize_google(audio))
+ 
         except Exception as e:
-            print("Error:" + str(e))
-
+            print("Error:  " + str(e))
+ 
+        # write audio
+        with open("recorded.wav", "wb") as f:
+            f.write(audio.get_wav_data())
+ 
 main()
