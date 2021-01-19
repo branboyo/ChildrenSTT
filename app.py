a7f5da988c925cce0b0b65f43eee0a2a8c9b41c4
Brandon Hsu <55924012+branboyo@users.noreply.github.com>
2021-01-19 15:10:13 -0500

    Fixed routing and added display for phrase evaluation

--- app.py
+++ app.py
@@ -20,29 +20,5 @@ def alpha():
 def apple():
     return render_template('apple.html')
 
-def check(string):
- 
-    r = sr.Recognizer()
- 
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
- 
-        print("Listening...")
- 
-        audio = r.listen(source, phrase_time_limit=3)
-        trans = r.recognize_google(audio)
-        try:
-            if (trans == string):
-                return True
-            else:
-                return False
- 
-        except Exception as e:
-            print("Error:  " + str(e))
-
-        # write audio
-        with open("recorded.wav", "wb") as f:
-            f.write(audio.get_wav_data())
-
 if __name__ == "__main__":
     app.run()
