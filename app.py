63df2082cf13b09336222e49e87f4cdace799fa6
Brandon Hsu <55924012+branboyo@users.noreply.github.com>
2021-01-24 16:56:17 -0500

    Finished working demo checkpoint

--- app.py
+++ app.py
@@ -11,24 +11,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('home.html')
+    return app.send_static_file('home.html')
 
 @app.route('/alpha')
 def alpha():
-    return render_template('alpha.html')
+    return app.send_static_file('alpha.html')
 
 @app.route('/alpha/apple')
 def apple():
-    return render_template('lesson.html', key = 'Apple', next = '/alpha/atlas', back = '/alpha', lesson = '1', part = '1', yt = 'https://www.youtube.com/embed/491-YZrgjxk?rel=0')
+    return render_template('lesson.html', key = 'APPLE', next = '/alpha/banana', back = '/alpha', lesson = '1', part = '1', yt = 'https://www.youtube.com/embed/491-YZrgjxk?rel=0')
 
-@app.route('/alpha/atlas')
-def atlas():
-    return render_template('lesson.html', key = 'Atlas', next = '/alpha/aunt', back = '/alpha/apple', lesson = '1', part = '2', yt = 'https://www.youtube.com/embed/r356xkret6I?rel=0' )
+@app.route('/alpha/banana')
+def banana():
+    return render_template('lesson.html', key = 'BANANA', next = '/alpha/close', back = '/alpha/apple', lesson = '1', part = '2', yt = 'https://www.youtube.com/embed/-ocYrqMe1ag?rel=0')
 
-@app.route('/alpha/aunt')
-def ant():
-    return render_template('lesson.html', key = 'aunt', next = '/unregistered', back = '/alpha/atlas', lesson = '1', part = '3', yt = 'aaa')
+@app.route('/alpha/close')
+def close():
+    return render_template('lesson.html', key = 'CLOSE', next = '/alpha/dog', back = '/alpha/banana', lesson = '1', part = '3', yt = 'https://www.youtube.com/embed/9SXr4uhafio?rel=0')
 
+@app.route('/alpha/dog')
+def dog():
+    return render_template('lesson.html', key = 'DOG', next = '/alpha/ear', back = '/alpha/close', lesson = '1', part = '4', yt = 'https://www.youtube.com/embed/P-ImIwb79sE?rel=0')
+
+@app.route('/alpha/ear')
+def ear():
+    return render_template('lesson.html', key = 'EAR', next = '/alpha/flower', back = '/alpha/dog', lesson = '1', part = '5', yt = 'https://www.youtube.com/embed/rb9z3iRN5E4?rel=0')
+
+@app.route('/alpha/flower')
+def flower():
+    return render_template('lesson.html', key = 'FLOWER', next = '/alpha/golf', back = '/alpha/ear', lesson = '1', part = '6', yt = 'https://www.youtube.com/embed/55ItxbQqVQM?rel=0')
+
+@app.route('/alpha/golf')
+def golf():
+    return render_template('lesson.html', key = 'GOLF', next = '/unregistered', back = '/alpha/flower', lesson = '1', part = '7', yt = 'https://www.youtube.com/embed/icVisKkTYb0?rel=0')
 
 
 if __name__ == "__main__":
